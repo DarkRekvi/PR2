1c9890a2492e69e58da74b38a710c0f819fc9eeb
Danil Privalov <privatnik15@gmail.com>
2023-04-20 02:35:42 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PR2Makso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1053, 328);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AMD",
+            "INTEL"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 346);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 558);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR2Makso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            MPlate Gigabyte1 = new MPlate("Gigabyte motherboard 120382");
+            MPlate Gigabyte2 = new MPlate("Gigabyte motherboard 456456");
+
+            PBlock Block1 = new PBlock("Block1", 200);
+            PBlock Block2 = new PBlock("Block2", 250);
+
+            Processor AMD1 = new Processor("AMD");
+            Processor AMD2 = new Processor("AMD");
+            Processor INTEL1 = new Processor("INTEL");
+            Processor INTEL2 = new Processor("INTEL");
+
+            SystemBlock SysBlock1 = new SystemBlock(Gigabyte1, Block1, AMD1);
+            SystemBlock SysBlock2 = new SystemBlock(Gigabyte1, Block1, AMD1);
+            SystemBlock SysBlock3 = new SystemBlock(Gigabyte1, Block1, INTEL1);
+            SystemBlock SysBlock4 = new SystemBlock(Gigabyte1, Block1, INTEL1);
+
+            List<SystemBlock> parts = new List<SystemBlock>();
+
+            parts.Add(SysBlock1);
+            parts.Add(SysBlock2);
+            parts.Add(SysBlock3);
+            parts.Add(SysBlock4);
+
+            listView1.Items.Add(SysBlock1.ProcName);
+            listView1.Items.Add(SysBlock2.ProcName);
+            listView1.Items.Add(SysBlock3.ProcName);
+            listView1.Items.Add(SysBlock4.ProcName);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    class MPlate
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine("Motherboard name: " + name);
+        }
+
+        public MPlate(string _name)
+        {
+            if (_name.Length >= 20 && _name.Length <= 150)
+            {
+                name = _name;
+            }
+
+            else
+            {
+                name = "defaultplatename";
+            }
+        }
+    }
+
+    class PBlock
+    {
+        private string name;
+        private int power;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Power
+        {
+            get { return power; }
+            set
+            {
+                power = value;
+            }
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine("Powerblock name: " + name + " " + power.ToString() + " W");
+        }
+
+        public PBlock(string _name, int _power)
+        {
+            name = _name;
+            power = _power;
+        }
+    }
+
+    class Processor
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine("Processor name: " + name);
+        }
+
+        public Processor(string _name)
+        {
+            name = _name;
+        }
+    }
+
+    class SystemBlock
+    {
+        public string PlateName;
+        public string BlockName;
+        public string ProcName;
+        public int BlockPower;
+        public SystemBlock (MPlate mp, PBlock pb, Processor pr)
+        {
+            PlateName = mp.Name;
+            BlockName = pb.Name;
+            ProcName = pr.Name;
+            BlockPower = pb.Power;
+        }
+    }
+}
+
